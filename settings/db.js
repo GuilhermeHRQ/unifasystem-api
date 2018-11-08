95dd42072c55c5f9762fea5d8cd4ac7368b892a9
GuilhermeHenrique_SMN <guilherme.henrique@smn.com.br>
2018-11-08 18:27:03 -0200

    connect db

--- settings/db.js
+++ settings/db.js
@@ -1,11 +1,9 @@
 const databaseConfig = {
-    'host': 'ec2-54-197-230-161.compute-1.amazonaws.com',
+    'host': '18.188.151.183',
     'port': 5432,
-    'database': 'df6vhct6ogge09',
-    'user': 'jfyucfiyvfztpy',
-    'password': '4523bdfc3acea28e579ecd2d10492be08bb8e4992f1d048e007c0fe1d992ea47',
-    'ssl': true,
-    'sslfactory': 'org.postgresql.ssl.NonValidatingFactory'
+    'database': 'unifasystem',
+    'user': 'postgres',
+    'password': '123321'
 };
 
 //const connectionString = 'postgres://jfyucfiyvfztpy:4523bdfc3acea28e579ecd2d10492be08bb8e4992f1d048e007c0fe1d992ea47@ec2-54-197-230-161.compute-1.amazonaws.com:5432/df6vhct6ogge09?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory';
