1fb465483d3ef077558ffaafc9200b63f2efdd19
Guilherme Henrique <guilherme.henrique@smn.com.br>
2018-11-13 21:50:19 -0200

    retirando verify do aluno-presenca

--- src/api/routes/alunoPresencaRoutes.js
+++ src/api/routes/alunoPresencaRoutes.js
@@ -2,5 +2,5 @@ const api = require('../../core/alunoPresenca/alunoPresencaController');
 const verify = global.verify;
 
 module.exports = (app) => {
-    app.route('/aluno-presenca/:idAluno').post(verify(api.inserirPresenca));
+    app.route('/aluno-presenca/:idAluno').post(api.inserirPresenca);
 };
